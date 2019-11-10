35e960eb57b499faff89514fe5bd62e698b55429
Danish Naglekar <dnaglekar@Addinge.com>
2019-11-10 17:22:48 -0500

    Added a control for D365 Saturday Portland 2019

---
+++ D365SatPortland/D365SatPortlandControl/index.ts
@@ -0,0 +1,110 @@
+import {IInputs, IOutputs} from "./generated/ManifestTypes";
+import { string } from "prop-types";
+
+export class D365SatPortlandControl implements ComponentFramework.StandardControl<IInputs, IOutputs> {
+
+	theNotifyOutputChanges: () => void;
+
+	/**
+	 * HTML Elements
+	 */
+	private eleMainContainer: HTMLDivElement;
+	private eleInputTextbox: HTMLTextAreaElement;
+	private eleOutputLabel: HTMLLabelElement;
+
+	/**
+	 * Event Listener
+	 */
+	private eventTextInputChanged: EventListenerOrEventListenerObject;
+
+	/**
+	 * Empty constructor.
+	 */
+	constructor()
+	{
+
+	}
+
+	/**
+	 * Used to initialize the control instance. Controls can kick off remote server calls and other initialization actions here.
+	 * Data-set values are not initialized here, use updateView.
+	 * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to property names defined in the manifest, as well as utility functions.
+	 * @param notifyOutputChanged A callback method to alert the framework that the control has new outputs ready to be retrieved asynchronously.
+	 * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
+	 * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
+	 */
+	public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container:HTMLDivElement)
+	{
+		this.theNotifyOutputChanges = notifyOutputChanged;
+		let currentTextValue = context.parameters.InputText.raw || "";
+
+		this.eventTextInputChanged = this.OnChange.bind(this);
+
+		// UI
+		this.eleMainContainer = document.createElement("div");
+		this.eleMainContainer.className = "main-container";
+
+		this.eleInputTextbox = document.createElement("textarea");
+		this.eleInputTextbox.value = currentTextValue;
+		this.eleInputTextbox.setAttribute("placeholder", "---");
+		this.eleInputTextbox.className = "my-textarea";
+		this.eleInputTextbox.addEventListener("keyup", this.eventTextInputChanged);
+		
+		this.eleOutputLabel = document.createElement("label");
+		this.eleOutputLabel.className = "my-label";
+
+		// Add containers
+		this.eleMainContainer.appendChild(this.eleInputTextbox);
+		this.eleMainContainer.appendChild(this.eleOutputLabel);
+		container.appendChild(this.eleMainContainer);
+
+		this.CalculateNumberOfCharacters();
+	}
+
+
+	/**
+	 * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
+	 * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
+	 */
+	public updateView(context: ComponentFramework.Context<IInputs>): void
+	{
+		// Add code to update control view
+	}
+
+	/** 
+	 * It is called by the framework prior to a control receiving new data. 
+	 * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as “bound” or “output”
+	 */
+	public getOutputs(): IOutputs
+	{
+		return {
+			InputText: this.eleInputTextbox.value
+		};
+	}
+
+	/** 
+	 * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
+	 * i.e. cancelling any pending remote calls, removing listeners, etc.
+	 */
+	public destroy(): void
+	{
+		this.eleInputTextbox.removeEventListener("keyup", this.eventTextInputChanged);
+	}
+
+	/***** PRIVATE FUNCTIONS *****/
+	private OnChange(evnt: Event): void 
+	{
+		this.CalculateNumberOfCharacters();
+	}
+
+	private CalculateNumberOfCharacters()
+	{
+		let charCount : string = (this.eleInputTextbox.value.length.toString() || "0");
+		let wordCount : string = (this.eleInputTextbox.value.split(' ').length.toString() || "0");
+
+		this.eleOutputLabel.innerHTML = charCount + (parseInt(charCount) <= 1 ? " character ": " characters");
+		this.eleOutputLabel.innerHTML += ", " + wordCount + (parseInt(wordCount) <= 1 ? " word " : " words");
+
+		this.theNotifyOutputChanges();
+	}
+}
